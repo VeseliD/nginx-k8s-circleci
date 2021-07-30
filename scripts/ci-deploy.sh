685b86c0432a2baab45613b0f46d53fa586c7491
Dragomir Veselinovic <veselidrago@gmail.com>
2021-07-30 15:13:28 +0200

    Updated CircleCI and Kubernetes config

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -10,8 +10,8 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/nginx-k8s-circleci.yml >./kube/nginx-k8s-circleci.yml.out
-mv ./kube/nginx-k8s-circleci.yml.out ./kube/nginx-k8s-circleci.yml
+envsubst <../kube/nginx-k8s-circleci.yml >../kube/nginx-k8s-circleci.yml.out
+mv ../kube/nginx-k8s-circleci.yml.out ../kube/nginx-k8s-circleci.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
